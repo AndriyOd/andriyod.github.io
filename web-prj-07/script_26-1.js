91b82c49c0fe652d11c128a002cb7de1ba3a744c
andriyod <andriy.kalyapin@gmail.com>
2022-06-12 19:59:48 +0300

    Smooth Scroll
    Smooth Scroll

--- web-prj-07/script_26-1.js
+++ web-prj-07/script_26-1.js
@@ -1,21 +1,57 @@
+// Auto-fillling paragraphs with "Lorem..." text
 let str = document.querySelector('#div01 > p').textContent;
-
 let pArr = document.querySelectorAll('p');
-
 pArr.forEach((item) => item.textContent = str);
 
-
-// let header5 = document.querySelector('#header-5');
-
-// let linkHeader5 = document.querySelector('#linkHeader-5');
-
-// linkHeader5.addEventListener('click', (event) => {
-//     event.preventDefault();
-//     header5.scrollIntoView({behavior: 'smooth'});
-//     // window.scroll({ top: 0, behavior: 'smooth' });
-// });
-
-document.querySelector('#menu-btn-container').addEventListener('click', (e)=>{
+// Navigation bar behaviour Listeners
+document.querySelector('#menu-btn-container').addEventListener('click', (e) => {
     document.querySelector('#menu-btn-container').classList.toggle('active');
     document.querySelector('#nav-plate').classList.toggle('active');
 });
+
+// Smooth Scroll to a target after nav-bar link clicking
+let scrollTargetCoordY = 0;
+let scrollTimer;
+
+let headerLinks = document.querySelectorAll('#menu-header a');
+headerLinks.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+        let targetId = document.querySelector(event.target.attributes.href.value);
+        let scrollTargetCoordY = targetId.offsetTop-80;
+
+        if (window.scrollY < scrollTargetCoordY) {
+            if (!scrollTimer) {
+                scrollTimer = setInterval(SmoothScrollDown, 20, scrollTargetCoordY);
+            }
+        }
+
+        if (window.scrollY > scrollTargetCoordY) {
+            if (!scrollTimer) {
+                scrollTimer = setInterval(SmoothScrollUp, 20, scrollTargetCoordY);
+            }
+        }
+    });
+});
+
+
+function SmoothScrollDown(coordY) {
+    window.scroll(0, window.scrollY+20);
+
+    if (window.scrollY >= coordY) {
+        clearInterval(scrollTimer);
+        scrollTimer = null; 
+    }
+}
+
+function SmoothScrollUp(coordY) {
+    window.scroll(0, window.scrollY-20);
+
+    if (window.scrollY <= coordY || window.scrollY <= 0) {
+        clearInterval(scrollTimer);
+        scrollTimer = null; 
+    }
+}
+
+
+
