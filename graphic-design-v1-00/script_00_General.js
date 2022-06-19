8e7c904caca22ae96f8b38fc3b8a3df225434923
andriyod <andriy.kalyapin@gmail.com>
2022-06-19 11:21:57 +0300

    Final assmebly, Rev_1-00, almost adaptive

--- graphic-design-v1-00/script_00_General.js
+++ graphic-design-v1-00/script_00_General.js
@@ -1,24 +1,23 @@
-// window.addEventListener('scroll', (event)=>{
-//     // console.log(window);
-//     // console.dir(window);
-//     let wscrollY = window.scrollY;
-//     let winnerHeight = window.innerHeight;
-//     document.querySelector('#wscrollTop').textContent = wscrollY;
-//     document.querySelector('#wheight').textContent = winnerHeight;
+// Hiding all the sections for the following revealing during scrolling
+let sections = [];
+sections[0] = document.querySelector('#slider-wrapper'); // PORTFOLIO Section
+sections[1] = document.querySelector('#grid-container'); // "TYPES OF DESIGN" Section
+sections[2] = document.querySelector('#table-container'); // PRICE CALCULATOR Section
+sections[3] = document.querySelector('#weather-grid-container'); // WEATHER FORECAST Section
 
-//     let objectOffsetTop = document.querySelector('#grid-container').offsetTop;
-//     let windowOffsetTop = wscrollY + winnerHeight;
-//     if (windowOffsetTop > objectOffsetTop) {
-//         console.log('We have just reached (touched) the object by bottom of the window');
-//     }
+sections.forEach((item)=>{
+    item.classList.add('hidden');
+});
 
-// });
+
 
 // Navigation bar behaviour Listeners
 document.querySelector('#menu-btn-container').addEventListener('click', (e) => {
     document.querySelector('#menu-btn-container').classList.toggle('active');
     document.querySelector('#nav-plate').classList.toggle('active');
 });
+
+
 
 // Smooth Scroll to a target after nav-bar link clicking
 let scrollTargetCoordY = 0;
@@ -28,16 +27,11 @@ let headerLinks = document.querySelectorAll('#menu-header a');
 headerLinks.forEach((item) => {
     item.addEventListener('click', (event) => {
         event.preventDefault();
-        // event.stopPropagation();
         let targetId = document.querySelector(event.target.attributes.href.value);
         let scrollTargetCoordY = targetId.offsetTop-80;
 
-        // console.dir(targetId.id);
-        // console.dir(typeof targetId.id);
-
         if (targetId.id == "hContacts") {
-            // console.log('In If block');
-            scrollTargetCoordY = document.querySelector('#hWeather').offsetTop;
+            scrollTargetCoordY = document.querySelector('#hWeather').offsetTop+5;
         }
 
         if (window.scrollY < scrollTargetCoordY) {
@@ -73,5 +67,30 @@ function SmoothScrollUp(coordY) {
     }
 }
 
+
+
+// Animation during scrolling the page
+window.addEventListener('scroll', (event)=>{
+    let wScrollY = window.scrollY;
+    let wHeight = window.innerHeight/2;
+    let windowOffsetTop = wScrollY + wHeight;
+   
+    sections.forEach((item) => {
+        let objectOffsetTop = item.offsetTop;
+
+        if (windowOffsetTop > objectOffsetTop) {
+            // console.log('We have just reached Target Object Section by center of the window');
+            item.classList.remove('hidden');
+        }
+
+    });
+
+
+    // // PORTFOLIO SECTION. Animated appearing 
+    // if (windowOffsetTop > objectOffsetTop) {
+    //     // console.log('We have just reached Target Object Section by center of the window');
+    //     slider.classList.remove('hidden');
+    // }
+});
 
 
